9fac8cf87c8a8e4b7cd2cfe3ab5dbd046b608a0b
shota.sakaguchi <shota.sakaguchi@tri-ad.global>
2021-05-02 21:37:55 +0900

    linepush

--- push_line_message.py
+++ push_line_message.py
@@ -3,11 +3,18 @@ import requests
 import subprocess
 from subprocess import PIPE
 
-def lineNotify(message):
-    line_notify_token = os.environ.get('LINE_TOKEN1')
-    line_notify_api = 'https://notify-api.line.me/api/notify'
-    payload = {'message': message}
-    headers = {'Authorization': 'Bearer ' + line_notify_token}
-    requests.post(line_notify_api, data=payload, headers=headers)
-proc = subprocess.run("downloadimage.sh", shell=True, text=True)
-lineNotify('(・ω・)')
+
+def lineNotify(message, *args):
+    line_notify_token = os.environ.get("LINE_TOKEN1")
+    line_notify_api = "https://notify-api.line.me/api/notify"
+    payload = {"message": message}
+    headers = {"Authorization": "Bearer " + line_notify_token}
+    if len(args) == 0:
+        requests.post(line_notify_api, data=payload, headers=headers)
+    else:
+        files = {"imageFile": open(args[0], "rb")}
+        requests.post(line_notify_api, data=payload, headers=headers, files=files)
+
+
+proc = subprocess.run("./downloadimage.sh", shell=True, text=True)
+lineNotify("(・ω・)", "./image.jpg")
